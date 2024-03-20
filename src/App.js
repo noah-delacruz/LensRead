b3ee679e8cf46d72e6b5e8d24a646732e2f81a3b
Noah dela Cruz <delacruznoah12898@yahoo.com>
2024-03-19 20:16:00 -0700

    Add take picture function

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import React from 'react'
+import React, { useCallback } from 'react'
 import { Button, Container, Typography } from '@mui/material';
 import Webcam from "react-webcam";
 
 const CustomWebcam = () => {
-  return (
-    <div className="container">
-      <Webcam height={600} width={600} />
-    </div>
-  );
+    const webcamRef = React.useRef(null)
+    const [imgSrc, setImgSrc] = React.useState(null)
+
+    // create a capture function
+    const takePicture = useCallback(() => {
+        const imageSrc = webcamRef.current.getScreenshot();
+        setImgSrc(imageSrc);
+    }, [webcamRef])
+
+    return (
+        <div className="container">
+            {imgSrc ? <img src={imgSrc} alt="camera picture" /> : <Webcam height={600} width={600} ref={webcamRef}/>}
+            <Button variant="contained" onClick={takePicture}>Take Picture</Button>
+        </div>
+    );
 };
 
 function App() {
@@ -19,7 +29,6 @@ function App() {
     <div>
         <Container maxWidth="sm" style={{ textAlign: 'center' }}>
             <Typography variant="h1" gutterBottom>LensRead</Typography>
-            <Button variant="contained">Click</Button>
             <CustomWebcam />
         </Container>
     </div>
