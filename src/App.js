b493e022586b8381de3fa3476d1a18aee8f496ab
Noah dela Cruz <delacruznoah12898@yahoo.com>
2024-03-19 20:06:53 -0700

    Add video stream capability

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import React from 'react'
 import { Button, Container, Typography } from '@mui/material';
+import Webcam from "react-webcam";
+
+const CustomWebcam = () => {
+  return (
+    <div className="container">
+      <Webcam height={600} width={600} />
+    </div>
+  );
+};
 
 function App() {
   return (
@@ -11,6 +20,7 @@ function App() {
         <Container maxWidth="sm" style={{ textAlign: 'center' }}>
             <Typography variant="h1" gutterBottom>LensRead</Typography>
             <Button variant="contained">Click</Button>
+            <CustomWebcam />
         </Container>
     </div>
   );
